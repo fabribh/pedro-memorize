b6cb1ea09f97552ec6ea24f8223e415cee027634
Fabio Pedro Ribeiro <si.fabioribeiro@gmail.com>
2020-07-10 22:55:50 -0300

    First part od the game is done

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,28 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Fabio Ribeiro on 10/07/20.
+//  Copyright © 2020 Fabribh. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻","🎃","🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
